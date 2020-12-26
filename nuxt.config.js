0aadb7cf9b2fe6010c22b7794473abb16a1ade25
cjty <1042048096@qq.com>
2020-12-26 17:07:10 +0800

    引入基础css,js,配置访问域名端口,

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,22 @@ module.exports = {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    script: [
+      { src: '/flexible.js', type: 'text/javascript', charset: 'utf-8' }
     ]
+  },
+  /*
+  * create all css
+  * ~ 通配符，来匹配
+  */
+  css:['~assets/css/base.css'],
+  /*
+  * 配置访问域名与端口
+  */
+  server: {
+    port: 8081, // default: 3000
+    host: '0.0.0.0' // default: localhost
   },
   /*
   ** Customize the progress bar color
@@ -21,6 +36,16 @@ module.exports = {
   ** Build configuration
   */
   build: {
+    loaders: [
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        loader: "url-loader",
+        query: {
+          limit: 10000,
+          name: "img/[name].[hash].[ext]"
+        }
+      }
+    ],
     /*
     ** Run ESLint on save
     */
